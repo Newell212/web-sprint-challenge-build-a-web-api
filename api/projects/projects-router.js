a4fd6b699b8ef8592f31aa93529cc12d7c949b2f
Michael Newell <michael.newell212@gmail.com>
2023-06-23 15:01:00 -0600

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,17 @@
-// Write your "projects" router here!
+const express = require('express');
+const {validateProjectId} = require('./projects-middleware');
+
+const Project = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Project.get()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
 const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
 
 server.use(express.json())
 
 server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send('<h2>its working<h2>')
