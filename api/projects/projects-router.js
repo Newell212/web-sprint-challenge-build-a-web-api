92e495bd6138f1bb30dbce35e4c3d8076592d4da
Michael Newell <michael.newell212@gmail.com>
2023-06-23 15:56:49 -0600

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -25,12 +25,13 @@ router.post('/', validateInfo, (req, res, next) => {
     .catch(next)
 })
 
-router.put('/:id', validateProjectId, validateInfo, (req, res, next) => {
-    Project.update(req.params.id, {name: req.name, description: req.description, completed: req.completed})
-    .then(updatedProj => {
-        res.json(updatedProj)
-    })
-    .catch(next)
+router.put('/:id', validateProjectId, validateInfo, async (req, res, next) => {
+   try {
+  const updatedProj = await Project.update(req.params.id, {completed: req.completed, description: req.description, name: req.name})
+    res.json(updatedProj)
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.delete('/:id', validateProjectId, async (req, res, next) => {
@@ -40,7 +41,16 @@ router.delete('/:id', validateProjectId, async (req, res, next) => {
     } catch(err) {
         next(err)
     }
-} )
+})
+
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+    try {
+        const result = await Project.getProjectActions(req.params.id)
+        res.json(result)
+    } catch(err) {
+        next(err)
+    }
+})
 
 
 module.exports = router;
